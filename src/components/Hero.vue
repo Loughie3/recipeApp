16003e20193f0adb282a6f9e6ed7b12299194dc3
Loughlin Craig <loughie5@hotmail.com>
2025-07-03 20:06:05 +1000

    first commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <div class="hero relative overflow-hidden h-[80vh]">
+      <img src="/Hero.jpg" alt="Hero Image" class="h-full w-full opacity-90" />
+      <div class="text-center absolute">
+        <div class="max-w-lg">
+          <h1 class="text-4xl font-bold py-6">
+            Recipes that are actually good
+          </h1>
+          <p>Tired of recipes that don't turn out well?</p>
+          <p>
+            This is a compilation of recipes that actually have flavour and
+            taste amazing.
+          </p>
+
+          <input type="text" placeholder="Search Recipe" class="input" />
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <Hero />
+  <main>
+    <h1 class="text-3xl text-center py-10">My Recipes</h1>
+    <div class="flex justify-center">
+      <div class="grid grid-cols-3 gap-5">
+        <div
+          v-for="recipe in recipes"
+          :key="recipe.id"
+          class="card mb-40 bg-base-100 border-2 w-96 shadow-sm"
+        >
+          <div class="card-body">
+            <h2 class="card-title">{{ recipe.title }}</h2>
+            <div class="card-actions justify-end"></div>
+          </div>
+          <figure>
+            <img :src="recipe.image_url" alt="Recipe image" />
+          </figure>
+          <button class="btn">View Recipe</button>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import Hero from "../components/Hero.vue";
+import { supabase } from "../lib/supabaseClient";
+
+export default {
+  name: "App",
+  components: {
+    Hero,
+  },
+  data() {
+    return {
+      recipes: [],
+    };
+  },
+  async mounted() {
+    const { data, error } = await supabase.from("recipe").select();
+    if (error) {
+      console.error("Error fetching recipes:", error);
+    } else {
+      this.recipes = data;
+    }
+  },
+};
+</script>
+
+<style></style>
